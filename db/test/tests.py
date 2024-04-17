21495638f16498c6a73b23eb9eb334748ac7ed09
Anatolyi Melnikov <eragon.monolyt@gmail.com>
2024-04-17 19:24:54 +0300

    added new tests

--- db/test/tests.py
+++ db/test/tests.py
@@ -1,14 +1,14 @@
 import unittest
-
 import numpy
-import pandas as pd
 from parameterized import parameterized
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
-from db.code.models import UserORM, Base, TitleORM, AuthorORM, ArtistORM, PublisherORM, TagORM, AbandonedTitleORM
-from db.code.queries import fill_table, add_categories, add_id_to_id_conn, get_id, add_full_title
+from ..everything.models import (UserORM, Base, TitleORM,
+                                 AuthorORM, ArtistORM, PublisherORM,
+                                 TagORM, AbandonedTitleORM, TitleTagORM)
+from ..everything.queries import (fill_table, add_categories, add_id_to_id_conn,
+                                  get_id, add_full_title, add_m2m_2)
 
 from .data import *
 
@@ -65,12 +65,14 @@ class TestQueries(unittest.TestCase):
             self.assertEqual(result.user_id, 1)
 
     def test_positive_get_id(self):
-        result = get_id(self.session_factory, AuthorORM, 'name')
-        self.assertEqual(1, result)
+        with self.session_factory() as session:
+            result = get_id(session, AuthorORM, 'name')
+            self.assertEqual(1, result)
 
     def test_negative_get_id(self):
-        result = get_id(self.session_factory, AuthorORM, numpy.nan)
-        self.assertEqual(None, result)
+        with self.session_factory() as session:
+            result = get_id(session, AuthorORM, numpy.nan)
+            self.assertEqual(None, result)
 
     def test_positive_add_full_title(self):
         add_full_title(self.session_factory, good_title_full_data)
@@ -82,6 +84,7 @@ class TestQueries(unittest.TestCase):
         add_full_title(self.session_factory, good_title_full_data)
         with self.session_factory() as session:
             result = session.query(AuthorORM).filter_by(id=1).first()
+
             self.assertEqual('Test_Author', result.name)
 
     def test_positive_connection_of_title_and_existing_author(self):
@@ -98,6 +101,21 @@ class TestQueries(unittest.TestCase):
             result = session.query(AuthorORM).filter_by(id=2).first()
             self.assertEqual(2, result.id)
 
-#TODO тест с загрузкой данных из файла
+    def test_positive_add_m2m(self):
+        add_full_title(self.session_factory, good_title_full_data)
+        with self.session_factory() as session:
+            title = session.query(TitleORM).first()
+            add_m2m_2(session,
+                      attr='tags',
+                      left_orm_object=title,
+                      right_orm_name=TagORM,
+                      data=good_title_full_data.iloc[0]['tags']
+                      )
+            result = session.query(TitleORM).filter_by(id=1).first()
+            expected = '[<TitleTagORM title_id=1, tag_id=1>, <TitleTagORM title_id=1, tag_id=2>]'
+            self.assertEqual(expected, str(session.query(TitleTagORM).all()))
+
+
+# TODO тест с загрузкой данных из файла
 if __name__ == '__main__':
     unittest.main()
