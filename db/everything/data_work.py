4d47117d0dc89c383d466eb95f589de70da7c606
Anatolyi Melnikov <eragon.monolyt@gmail.com>
2024-04-17 19:22:17 +0300

    updated expand_manga_data and added 2 functions to clean data

--- db/everything/data_work.py
+++ db/everything/data_work.py
@@ -1,18 +1,21 @@
 import csv
 import os
 import sys
-import pandas as pd
+from typing import Any
 
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import numpy as np
+import pandas as pd
+from config import DATA_DIR
 
 
 # TODO унифицировтаь под похожие случаи
 def get_authors() -> pd.DataFrame:
-    data = pd.read_csv('../../data/manga_data_expanded.csv')
+    data = pd.read_csv(DATA_DIR + 'manga_data_expanded.csv')
     result = []
     for row in data[['url', 'authors']].itertuples():
-        for author in row.authors.split('\n'):
-            result.append((row.url, author))
+        if not pd.isnull(row.authors):
+            for author in row.authors.split('\n'):
+                result.append((row.url, author))
     result = pd.DataFrame(result, columns=['title_url', 'author_name'])
 
     return result
@@ -29,13 +32,13 @@ def change_sex(sex: str) -> int:
 
 def get_titles_url() -> pd.DataFrame:
     unique_titles = set()
-    file_names = ['../../data/favorite_titles.csv', '../../data/abandoned_titles.csv']
+    file_names = [DATA_DIR + 'favorite_titles.csv', DATA_DIR + 'abandoned_titles.csv']
     for file_name in file_names:
         with open(file_name) as file:
             csv_reader = csv.reader(file)
             for line in csv_reader:
                 unique_titles.add(line[1].split('/')[-1])
-    titles_df = pd.read_csv('../../data/manga_data_expanded.csv')
+    titles_df = pd.read_csv(DATA_DIR + 'manga_data_expanded.csv')
     titles_urls = list(titles_df['url'])
     unique_titles.update(titles_urls)
     unique_titles = pd.DataFrame(unique_titles, columns=['url'])
@@ -58,7 +61,7 @@ def get_pairs(file_name: str) -> pd.DataFrame:
 
 def get_user_data() -> pd.DataFrame:
     columns = ['url', 'sex', 'favorite_genres']
-    data = pd.read_csv('../../data/users_info.csv',
+    data = pd.read_csv(DATA_DIR + 'users_info.csv',
                        header=None,
                        names=columns,
                        dtype='str').drop_duplicates(subset=['url'])
@@ -89,7 +92,7 @@ def set_name(old_names: pd.Series, new_names: pd.Series) -> list:
 
 
 def get_unique_values() -> None:
-    data = pd.read_csv('../data/manga_data_expanded.csv')
+    data = pd.read_csv(DATA_DIR + 'manga_data_expanded.csv')
     cols = ['authors', 'artists', 'publishers']
     authors = set()
     artists = set()
@@ -99,13 +102,32 @@ def get_unique_values() -> None:
         artists.update(t.artists.split('\n'))
         publishers.update(t.publishers.split('\n'))
 
-    pd.Series(list(authors)).to_csv('../data/authors.csv', encoding='UTF-8', index=False, header=False)
-    pd.Series(list(artists)).to_csv('../data/artists.csv', encoding='UTF-8', index=False, header=False)
-    pd.Series(list(publishers)).to_csv('../data/publishers.csv', encoding='UTF-8', index=False, header=False)
+    pd.Series(list(authors)).to_csv(DATA_DIR + 'authors.csv', encoding='UTF-8', index=False, header=False)
+    pd.Series(list(artists)).to_csv(DATA_DIR + 'artists.csv', encoding='UTF-8', index=False, header=False)
+    pd.Series(list(publishers)).to_csv(DATA_DIR + 'publishers.csv', encoding='UTF-8', index=False,
+                                       header=False)
 
 
-def expand_manga_data() -> None:
-    data = pd.read_csv('../../data/manga_data.csv')
+def remove_duplicates_from_list(data: pd.Series) -> list:
+    result = []
+    for value in data.values:
+        if value:
+            result.append(set(map(lambda x: x.lower(), value.split('\n'))))
+        else:
+            result.append(set())
+    return result
+
+
+def fix_year(year: str) -> str:
+    if year and len(year) > 4:
+        return year[:4]
+    else:
+        return year
+
+
+def expand_manga_data(regime: str = 'return') -> Any:
+    data = pd.read_csv(DATA_DIR + 'manga_data.csv')
+    data = data.drop_duplicates()
     cols = ["Год релиза", "Статус тайтла", 'Статус перевода',
             'Автор', 'Художник', "Издательство", 'Загружено глав', "Возрастной рейтинг", "Формат выпуска"]
     info_list = data['info_list']
@@ -122,21 +144,27 @@ def expand_manga_data() -> None:
                                 'Загружено глав': 'chapters_uploaded',
                                 'Возрастной рейтинг': 'age_rating',
                                 'link': 'url',
-                                'Формат выпуска': 'release_format'})
+                                'Формат выпуска': 'release_formats'})
     data['name'] = set_name(data['name'], data['alt_name'])
     data.drop(columns=['alt_name', 'info_list'], inplace=True)
     # na_cols = ['publication_status', 'translation_status',
     #            'authors', 'artists', 'publishers', 'chapters_uploaded', 'age_rating', 'year']
     # for na_col in na_cols:
     #     data[na_col] = data[na_col].fillna('Unknown')
-
-    data.to_csv('../../data/manga_data_expanded.csv', encoding='UTF-8', index=False)
-
+    m2m_cols = ['publishers', 'authors', 'artists', 'release_formats']
+    data = data.replace(np.nan, None)
+    for m2m_col in m2m_cols:
+        data[m2m_col] = remove_duplicates_from_list(data[m2m_col])
+    data['release_year'] = data['release_year'].apply(fix_year)
+    if regime == 'return':
+        return data
+    else:
+        data.to_csv(DATA_DIR + 'manga_data_expanded.csv', encoding='UTF-8', index=False)
 
 
 def get_abandoned_titles() -> pd.DataFrame:
     columns = ['user_url', 'title_url']
-    data = pd.read_csv('../data/abandoned_titles.csv',
+    data = pd.read_csv(DATA_DIR + 'abandoned_titles.csv',
                        header=None,
                        names=columns,
                        dtype='str')
@@ -145,7 +173,7 @@ def get_abandoned_titles() -> pd.DataFrame:
 
 def get_favorite_titles() -> pd.DataFrame:
     columns = ['user_url', 'title_url']
-    data = pd.read_csv('../backup/favorite_titles.csv',
+    data = pd.read_csv(DATA_DIR + 'favorite_titles.csv',
                        header=None,
                        names=columns,
                        dtype='str')
@@ -153,9 +181,8 @@ def get_favorite_titles() -> pd.DataFrame:
 
 
 def save_csv(data: dict) -> None:
-    file_name = f'data/users_and_titles.csv'
+    file_name = DATA_DIR + 'users_and_titles.csv'
 
     df = pd.DataFrame.from_records(data,
                                    columns=['user_id', 'user_link', 'title_id', 'title_link'])
     df.to_csv(file_name, index=False)
-
