629165ecbb537f1c4ee87d1f14a7a28352291936
Anatolyi Melnikov <eragon.monolyt@gmail.com>
2024-04-19 18:10:47 +0300

    Base class moved to models.py

--- db/everything/models.py
+++ db/everything/models.py
@@ -1,11 +1,23 @@
 from sqlalchemy import String, ForeignKey, SmallInteger
-from typing import Annotated
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-
-from .database import Base
+from sqlalchemy.orm import DeclarativeBase
+from typing import Annotated
 
 
 int_pk = Annotated[int, mapped_column(primary_key=True)]
+
+
+class Base(DeclarativeBase):
+    def __repr__(self):
+        """Relationships не используются в repr(), т.к. могут вести к неожиданным подгрузкам"""
+        self.repr_cols_num = 3
+        self.repr_cols = tuple()
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
 
 
 class UserORM(Base):
