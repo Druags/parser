68002af9fe2ec3148ddd5fbe34b998cbc6dd47e7
Anatolyi Melnikov <eragon.monolyt@gmail.com>
2024-04-19 18:12:09 +0300

    imports changed from relative to absolute

--- db/test/tests.py
+++ db/test/tests.py
@@ -1,16 +1,16 @@
 import unittest
+
 import numpy
 from parameterized import parameterized
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from ..everything.models import (UserORM, Base, TitleORM,
-                                 AuthorORM, ArtistORM, PublisherORM,
-                                 TagORM, AbandonedTitleORM, TitleTagORM)
-from ..everything.queries import (fill_table, add_categories, add_id_to_id_conn,
-                                  get_id, add_full_title, add_m2m_2)
-
-from .data import *
+from db.everything.models import (UserORM, Base, TitleORM,
+                                  AuthorORM, ArtistORM, PublisherORM,
+                                  TagORM, AbandonedTitleORM, TitleTagORM)
+from db.everything.queries import (fill_table, add_categories, add_id_to_id_conn,
+                                   get_id, add_full_title, add_m2m)
+from db.test.data import *
 
 
 class TestQueries(unittest.TestCase):
@@ -105,13 +105,13 @@ class TestQueries(unittest.TestCase):
         add_full_title(self.session_factory, good_title_full_data)
         with self.session_factory() as session:
             title = session.query(TitleORM).first()
-            add_m2m_2(session,
-                      attr='tags',
-                      left_orm_object=title,
-                      right_orm_name=TagORM,
-                      data=good_title_full_data.iloc[0]['tags']
-                      )
-            result = session.query(TitleORM).filter_by(id=1).first()
+            add_m2m(session,
+                    b_p_field='tags',
+                    main_object=title,
+                    right_orm_name=TagORM,
+                    data=good_title_full_data.iloc[0]['tags']
+                    )
+
             expected = '[<TitleTagORM title_id=1, tag_id=1>, <TitleTagORM title_id=1, tag_id=2>]'
             self.assertEqual(expected, str(session.query(TitleTagORM).all()))
 
