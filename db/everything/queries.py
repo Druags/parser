8c5dec4553211cc985c8c0ffc6a4735cdd59a811
Anatolyi Melnikov <eragon.monolyt@gmail.com>
2024-04-27 16:46:07 +0300

    new function add_ratings. removed separate creation of categories

--- db/everything/queries.py
+++ db/everything/queries.py
@@ -4,13 +4,13 @@ from sqlalchemy.sql.expression import func
 from sqlalchemy.orm.session import sessionmaker, Session
 from tqdm import tqdm
 
-from db.everything.data_work import expand_manga_data
+from db.everything.data_work import expand_manga_data, get_user_info
 from db.everything.models import (Base, UserORM, TitleORM, TagORM,
                                   AuthorORM, ArtistORM, PublisherORM,
                                   TranslationStatusORM, ReleaseFormatORM,
-                                  PublicationStatusORM, AgeRatingORM, TypeORM)
+                                  PublicationStatusORM, AgeRatingORM, TypeORM, TitleRatingORM)
 from db.everything import data
-from db.everything.dataclasses import TableContent, TableRelation
+from db.everything.data_classes import TableContent, TableRelation
 from config import DATA_DIR
 
 
@@ -23,16 +23,21 @@ def fill_tables(session_factory: sessionmaker) -> None:
     tables_content = (TableContent(AuthorORM, data.authors),
                       TableContent(ArtistORM, data.artists),
                       TableContent(PublisherORM, data.publishers),
-                      TableContent(TagORM, data.tags))
+                      TableContent(TagORM, data.tags),
+                      TableContent(TypeORM, data.title_types),
+                      TableContent(PublicationStatusORM, data.publication_statuses),
+                      TableContent(AgeRatingORM, data.age_ratings),
+                      TableContent(TranslationStatusORM, data.translation_statuses),
+                      TableContent(ReleaseFormatORM, data.release_formats)
+                      )
 
     for orm_name, table_content in tables_content:
         df_to_orm(session_factory, orm_name=orm_name, converted_data=table_content)
-    add_categories(session_factory)
 
     titles = expand_manga_data()
     add_full_title(session_factory, titles)
 
-    user_data = pd.read_csv(DATA_DIR + 'users_full_info.csv')
+    user_data = get_user_info(DATA_DIR + 'users_full_info.csv')
     add_full_user(session_factory, user_data)
 
 
@@ -42,10 +47,10 @@ def add_full_user(session_factory: sessionmaker, users_info: pd.DataFrame) -> No
             user = UserORM(url=getattr(row, 'url'),
                            sex=getattr(row, 'sex'))
             session.add(user)
-
             m2m_relations = (
                 TableRelation(TitleORM, 'favorite_titles', getattr(row, 'favorite_titles')),
-                TableRelation(TitleORM, 'abandoned_titles', getattr(row, 'abandoned_titles'))
+                TableRelation(TitleORM, 'abandoned_titles', getattr(row, 'abandoned_titles')),
+                TableRelation(TagORM, 'favorite_tags', getattr(row, 'favorite_tags'), 'name')
             )
 
             for element in m2m_relations:
@@ -54,7 +59,7 @@ def add_full_user(session_factory: sessionmaker, users_info: pd.DataFrame) -> No
                         join_orm_name=element.orm_name,
                         b_p_field=element.bp_field_name,
                         join_records=element.join_records,
-                        key_field_name='url')
+                        key_field_name=element.key_field_name)
 
         session.flush()
         session.commit()
@@ -66,14 +71,18 @@ def add_full_title(session_factory: sessionmaker, titles_info: pd.DataFrame) -> 
             title = TitleORM(url=getattr(row, 'url'),
                              release_year=getattr(row, 'release_year'),
                              chapters_uploaded=getattr(row, 'chapters_uploaded'),
-                             type=get_id(session, TypeORM, {'name': getattr(row, 'type')}),
-                             age_rating=get_id(session, AgeRatingORM, {'name': getattr(row, 'age_rating')}),
+                             type=get_id(session,
+                                         orm_name=TypeORM,
+                                         key_field_value=getattr(row, 'type')),
+                             age_rating=get_id(session,
+                                               orm_name=AgeRatingORM,
+                                               key_field_value=getattr(row, 'age_rating')),
                              translation_status=get_id(session,
-                                                       TranslationStatusORM,
-                                                       {'name': getattr(row, 'translation_status')}),
+                                                       orm_name=TranslationStatusORM,
+                                                       key_field_value=getattr(row, 'translation_status')),
                              publication_status=get_id(session,
-                                                       PublicationStatusORM,
-                                                       {'name': getattr(row, 'publication_status')})
+                                                       orm_name=PublicationStatusORM,
+                                                       key_field_value=getattr(row, 'publication_status'))
                              )
             session.add(title)
             m2m_relations = (
@@ -91,8 +100,19 @@ def add_full_title(session_factory: sessionmaker, titles_info: pd.DataFrame) -> 
                         b_p_field=element.bp_field_name,
                         join_records=element.join_records)
 
+            add_ratings(title, getattr(row, 'ratings'))
+
         session.flush()
         session.commit()
+
+
+def add_ratings(main_object, join_data: dict):
+    new_ratings = {}
+    for rating in join_data:
+        new_ratings[f'qty_{rating}'] = join_data[rating]
+
+    title_ratings = TitleRatingORM(title_id=main_object.id, **new_ratings)
+    main_object.ratings = title_ratings
 
 
 def add_m2m_to_existing(session: Session, *,
@@ -125,39 +145,23 @@ def add_m2m(session: Session, *,
         (getattr(main_object, b_p_field).
          append(session.query(join_orm_name).
                 filter_by(id=get_id(session,
-                                    join_orm_name,
-                                    {key_field_name: record_key})).first()
+                                    orm_name=join_orm_name,
+                                    key_field_name=key_field_name,
+                                    key_field_value=record_key)).first()
                 )
          )
 
 
-def get_id(session: Session, orm_name: Type[Base], key_n_a_v: dict):
-    """
-    :param session:
-    :param orm_name:
-    :param key_n_a_v: takes dict {field_name: field_value}
-    :return:
-    """
-    if pd.isnull(key_n_a_v):
+def get_id(session: Session, *, orm_name: Type[Base], key_field_value: str, key_field_name: str = 'name'):
+    if pd.isnull(key_field_value):
         return
     try:
-        return session.query(orm_name).filter_by(**key_n_a_v).first().id
+        return session.query(orm_name).filter_by(**{key_field_name: key_field_value}).first().id
     except AttributeError:
-        session.add(orm_name(**key_n_a_v))
+        session.add(orm_name(**{key_field_name: key_field_value}))
         max_id = session.query(func.max(orm_name.id)).first()[0]
 
         return max_id
-
-
-def add_categories(session_factory: sessionmaker) -> None:
-    cat_data = [TableContent(TypeORM, data.title_types),
-                TableContent(PublicationStatusORM, data.publication_statuses),
-                TableContent(AgeRatingORM, data.age_ratings),
-                TableContent(TranslationStatusORM, data.translation_statuses),
-                TableContent(ReleaseFormatORM, data.release_formats)
-                ]
-    for element in cat_data:
-        df_to_orm(session_factory, orm_name=element.orm_name, converted_data=element.content)
 
 
 def df_to_orm(session_factory: sessionmaker,
@@ -169,30 +173,3 @@ def df_to_orm(session_factory: sessionmaker,
                          for row in converted_data.itertuples()])
         session.flush()
         session.commit()
-
-
-def select_orm(session_factory: sessionmaker,
-               *,
-               orm_name: Type[Base],
-               index_name: str) -> pd.DataFrame:
-    with session_factory() as session:
-        result = session.query(orm_name).all()
-        result = pd.DataFrame.from_records([ob.to_dict() for ob in result], index='id')
-        result[index_name] = result.index
-        return result
-
-# def test_query(session_factory: sessionmaker):
-#     data = pd.read_csv(DATA_DIR + 'users_full_info.csv')
-#     with session_factory() as session:
-#         add_m2m_to_existing(session,
-#                             main_orm_name=UserORM,
-#                             b_p_field='favorite_titles',
-#                             join_orm_name=TitleORM,
-#                             data=data[['id', 'favorite_titles']].set_index(data['id']))
-#         add_m2m_to_existing(session,
-#                             main_orm_name=UserORM,
-#                             b_p_field='abandoned_titles',
-#                             join_orm_name=TitleORM,
-#                             data=data[['id', 'abandoned_titles']].set_index(data['id']))
-#         session.flush()
-#         session.commit()
